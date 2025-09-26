6abfbb5a53f5668730a8814ba90547314a023cdf
EX_THK <2517278658@qq.com>
2025-09-27 00:40:04 +0800

    添加了tranformer、残差网络，其中残差部分还未结合FiLM，FiLM的网络模块需要考虑

---
+++ model/resnet.py
@@ -0,0 +1,103 @@
+import torch
+
+import torch.nn as nn
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+        
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        out += self.shortcut(residual)
+        out = self.relu(out)
+        
+        return out
+    
+
+class FiLM(nn.Module):
+    def __init__(self, num_features, condition_size):
+        super(FiLM, self).__init__()
+        self.num_features = num_features
+        self.condition_network = nn.Linear(condition_size, num_features * 2)
+
+    def forward(self, x, condition):
+        # condition shape: [batch_size, condition_size]
+        # x shape: [batch_size, num_features, height, width]
+        film_params = self.condition_network(condition)
+        gamma, beta = torch.chunk(film_params, 2, dim=1)
+                
+        # Reshape for broadcasting
+        gamma = gamma.unsqueeze(-1).unsqueeze(-1)
+        beta = beta.unsqueeze(-1).unsqueeze(-1)
+                
+        # Apply FiLM transformation
+        return gamma * x + beta
+    
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        
+        self.layer1 = self.make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
+        
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        
+        return out
+
+def ResNet18(num_classes=10):
+    return ResNet(ResidualBlock, [2, 2, 2, 2], num_classes)
+
+def ResNet34(num_classes=10):
+    return ResNet(ResidualBlock, [3, 4, 6, 3], num_classes)
